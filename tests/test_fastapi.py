3eb13fe2b99d70bd216a6f26146a377c1ba0b906
Vanessa Dickey <vcdickey@gmail.com>
2023-11-06 19:23:55 +0000

    tres test passes

--- tests/test_fastapi.py
+++ tests/test_fastapi.py
@@ -14,3 +14,9 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+def test_read_item():
+    tres = 3
+    response = client.get("/items/" + str(tres))
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 3}
